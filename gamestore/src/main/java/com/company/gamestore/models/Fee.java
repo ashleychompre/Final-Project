210f7e4f7cfb19221f832d9b3b3ef79d7fedc5e8
Ashley Chompre <anc67@duke.edu>
2023-03-27 19:10:40 -0400

    adding tax and fee models

---
+++ gamestore/src/main/java/com/company/gamestore/models/Fee.java
@@ -0,0 +1,52 @@
+package com.company.gamestore.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Fee implements Serializable{
+
+    @Id
+    @Column(name="product_type")
+    private String product_type;
+    private BigDecimal fee;
+
+    public String getProductType() {
+        return product_type;
+    }
+
+    public void setProductType(String product_type) {
+        this.product_type = product_type;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fee fee1 = (Fee) o;
+        return product_type.equals(fee1.product_type) && fee.equals(fee1.fee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product_type, fee);
+    }
+
+    @Override
+    public String toString() {
+        return "Fee{" +
+                "product_type='" + product_type + '\'' +
+                ", fee=" + fee +
+                '}';
+    }
+}
