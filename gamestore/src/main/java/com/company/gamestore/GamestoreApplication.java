d816339aad9d217c327391dbb611b33c50f92d3e
zulymargs <zulymargs@gmail.com>
2023-03-29 18:38:22 -0400

    controllers/comments

--- gamestore/src/main/java/com/company/gamestore/GamestoreApplication.java
+++ gamestore/src/main/java/com/company/gamestore/GamestoreApplication.java
@@ -1,3 +1,14 @@
+/**-------------------------------------------------------------------------
+					 Final Project: Game Store Server
+ -Back-end API (via REST and GraphQL) inventory management web
+ service for a video game store.
+
+Authors:
+ Ashley Chompre, Oriyomi Oluwaseun Adeliyi, and Zulymar García Sonera
+
+ --------------------------------------------------------------------------*/
+
+
 package com.company.gamestore;
 
 import org.springframework.boot.SpringApplication;
