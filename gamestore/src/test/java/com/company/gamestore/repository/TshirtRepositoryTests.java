a6ae2e7194f21bde02802cdbd7fecaf48eb475a8
oadeliyi1 <oriyomiadeliyi@gmail.com>
2023-03-30 16:10:13 -0400

    Revert "Add tshirt repo test add method"
    This reverts commit a7d64abb8c4f7021e5bdb437909b9e3d65102446.

--- gamestore/src/test/java/com/company/gamestore/repository/TshirtRepositoryTests.java
+++ gamestore/src/test/java/com/company/gamestore/repository/TshirtRepositoryTests.java
@@ -1,56 +1,6 @@
 package com.company.gamestore.repository;
-import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
-import com.company.gamestore.models.TShirt;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class TshirtRepositoryTests {
-
-
-    @Autowired
-    TShirtRepository tshirtRepo;
-
-    @Before
-    public void setUp() throws Exception {
-        tshirtRepo.deleteAll();
-    }
-
-    @Test
-    public void addTshirt() {
-        //Arrange...
-        TShirt tshirt = new TShirt();
-         tshirt.setColor("black");
-         tshirt.setPrice(BigDecimal.valueOf(15.99));
-         tshirt.setSize("Medium");
-         tshirt.setQuantity(300);
-          tshirt.setDescription("Leopard print graphics");
-
-
-
-        tshirt = tshirtRepo.save(tshirt);
-
-        //Assert...
-        Optional<TShirt> customer1 = tshirtRepo.findById(tshirt.getId());
-
-        assertEquals(customer1.get(), tshirt);
-    }
-
-
-
 
 
 }
