432dfb72df296f3db1b1e41996acc78fa7f71307
Ashley Chompre <108883242+ashleychompre@users.noreply.github.com>
2023-04-02 14:50:47 -0400

    beginning to implement Service Layer

---
+++ gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
@@ -0,0 +1,32 @@
+package com.company.gamestore.service;
+
+import com.company.gamestore.repository.ConsoleRepository;
+import com.company.gamestore.repository.GameRepository;
+import com.company.gamestore.repository.TShirtRepository;
+import com.company.gamestore.models.Console;
+import com.company.gamestore.models.Game;
+import com.company.gamestore.models.Invoice;
+import com.company.gamestore.models.Tax;
+import com.company.gamestore.models.TShirt;
+import com.company.gamestore.models.Fee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.*;
+
+@Component
+public class ServiceLayer {
+
+    private ConsoleRepository consoleRepository;
+    private GameRepository gameRepository;
+    private TShirtRepository tshirtRepository;
+
+    @Autowired
+    public ServiceLayer(ConsoleRepository consoleRepository, GameRepository gameRepository,
+                        TShirtRepository tshirtRepository) {
+        this.consoleRepository = consoleRepository;
+        this.gameRepository = gameRepository;
+        this.tshirtRepository = tshirtRepository;
+    }
+
+}
