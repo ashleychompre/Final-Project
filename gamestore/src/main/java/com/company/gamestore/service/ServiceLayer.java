8a784f9a7c26706a5383bfb1c8b5e5069d14ec32
Ashley Chompre <108883242+ashleychompre@users.noreply.github.com>
2023-04-03 18:36:38 -0400

    service layer

--- gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
+++ gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
@@ -2,6 +2,7 @@ package com.company.gamestore.service;
 
 import com.company.gamestore.repository.ConsoleRepository;
 import com.company.gamestore.repository.GameRepository;
+import com.company.gamestore.repository.InvoiceRepository;
 import com.company.gamestore.repository.TShirtRepository;
 import com.company.gamestore.models.Console;
 import com.company.gamestore.models.Game;
@@ -9,6 +10,7 @@ import com.company.gamestore.models.Invoice;
 import com.company.gamestore.models.Tax;
 import com.company.gamestore.models.TShirt;
 import com.company.gamestore.models.Fee;
+import com.company.gamestore.viewmodel.InvoiceViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,12 +23,15 @@ public class ServiceLayer {
     private GameRepository gameRepository;
     private TShirtRepository tshirtRepository;
 
+    private InvoiceRepository invoiceRepository;
+
     @Autowired
     public ServiceLayer(ConsoleRepository consoleRepository, GameRepository gameRepository,
-                        TShirtRepository tshirtRepository) {
+                        TShirtRepository tshirtRepository, InvoiceRepository invoiceRepository) {
         this.consoleRepository = consoleRepository;
         this.gameRepository = gameRepository;
         this.tshirtRepository = tshirtRepository;
+        this.invoiceRepository = invoiceRepository;
     }
 
     public Invoice findInvoice(int id) {
@@ -41,11 +46,27 @@ public class ServiceLayer {
         return null;
     }
 
+
     public Invoice saveInvoice(Invoice invoice) {
 
         return null;
     }
 
+    private InvoiceViewModel buildInvoiceViewModel(Invoice invoice) {
+        InvoiceViewModel ivm = new InvoiceViewModel();
+        ivm.setId(invoice.getId());
+        ivm.setName(invoice.getName());
+        ivm.setCity(invoice.getCity());
+        ivm.setStreet(invoice.getStreet());
+        ivm.setState(invoice.getState());
+        ivm.setZipcode(invoice.getZipcode());
+        ivm.setItem_type(invoice.getItem_type());
+        ivm.setItem_id(invoice.getItem_id());
+        ivm.setQuantity(invoice.getQuantity());
+
+        return null;
+        
+    }
 
 
 }
