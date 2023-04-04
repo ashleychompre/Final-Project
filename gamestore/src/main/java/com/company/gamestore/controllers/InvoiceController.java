a263b687218d04def5a754830e136e855e2f0277
Ashley Chompre <108883242+ashleychompre@users.noreply.github.com>
2023-04-04 13:51:04 -0400

    finished invoice/service layer

--- gamestore/src/main/java/com/company/gamestore/controllers/InvoiceController.java
+++ gamestore/src/main/java/com/company/gamestore/controllers/InvoiceController.java
@@ -2,6 +2,7 @@ package com.company.gamestore.controllers;
 
 import com.company.gamestore.models.Invoice;
 import com.company.gamestore.service.ServiceLayer;
+import com.company.gamestore.viewmodel.InvoiceViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,24 +16,24 @@ public class InvoiceController {
     ServiceLayer serviceLayer;
 
     @GetMapping("/invoices")
-    public List<Invoice> getInvoices() {
+    public List<InvoiceViewModel> getInvoices() {
         return serviceLayer.findAllInvoices();
     }
 
     @GetMapping("/invoices/{id}")
-    public Invoice getInvoiceById(@PathVariable int id) {
+    public InvoiceViewModel getInvoiceById(@PathVariable int id) {
         return serviceLayer.findInvoice(id);
     }
 
     @GetMapping("/invoices/{name}")
-    public Invoice getInvoiceByName(@PathVariable String name) {
+    public List<InvoiceViewModel> getInvoiceByName(@PathVariable String name) {
         return serviceLayer.findInvoicebyName(name);
     }
 
     @PostMapping("/invoices")
     @ResponseStatus(HttpStatus.CREATED)
-    public Invoice addInvoice(@RequestBody Invoice invoice) {
-        return serviceLayer.saveInvoice(invoice);
+    public InvoiceViewModel addInvoice(@RequestBody InvoiceViewModel invoiceViewModel) {
+        return serviceLayer.saveInvoice(invoiceViewModel);
     }
 
 }
--- gamestore/src/main/java/com/company/gamestore/repository/InvoiceRepository.java
+++ gamestore/src/main/java/com/company/gamestore/repository/InvoiceRepository.java
@@ -5,11 +5,11 @@ import com.company.gamestore.models.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 
-    List<Invoice> findByName(String name);
+   List<Invoice> findByName(String name);
 
 }
