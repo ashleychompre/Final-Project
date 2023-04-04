236a428d65933d52b17152a6c8b03e4b70cbe98c
Ashley Chompre <108883242+ashleychompre@users.noreply.github.com>
2023-04-04 14:56:37 -0400

    serviceLayer Tests finished

--- gamestore/src/test/java/com/company/gamestore/controller/InvoiceControllerTest.java
+++ gamestore/src/test/java/com/company/gamestore/controller/InvoiceControllerTest.java
@@ -1,7 +1,36 @@
 package com.company.gamestore.controller;
 
-public class InvoiceControllerTest {
+import com.company.gamestore.controllers.ConsoleController;
+import com.company.gamestore.models.Console;
+import com.company.gamestore.models.Invoice;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ConsoleController.class)
+public class InvoiceControllerTest {
+    // Wiring in the MockMvc object
+    @Autowired
+    private MockMvc mockMvc;
+
+    // ObjectMapper used to convert Java objects to JSON and vice versa
+    private ObjectMapper mapper = new ObjectMapper();
+
+    // A list of records for testing purposes
+    private List<Invoice> invoiceList;
+
+    @Before
+    public void setUp() {
+        // Standard set up method, for instantiating test objects
+        // Don't have to do anything special for mockMvc since it's Autowired
+    }
 
 
 }
